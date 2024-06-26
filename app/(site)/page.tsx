4c4eef134ee2f6e4bbc46c1850133d833a275aa2
ayowaaab <ayoub.dahmen@esprit.tn>
2024-06-26 16:03:36 +0100

    🚀 Initial Commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100 ">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Home;
