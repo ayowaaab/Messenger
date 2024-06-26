4e3eaf261e556d35e36e826fef47c2be2eb34f42
ayowaaab <ayoub.dahmen@esprit.tn>
2024-06-26 18:18:14 +0100

    ✅ Added the authentification Form Design

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,26 @@
+import Image from "next/image";
 import React from "react";
+import AuthForm from "./components/AuthForm";
 
 const Home = () => {
   return (
-    <div className=" flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100 ">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <div className=" flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100 ">
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <Image
+            className="mx-auto w-auto"
+            src="/img/logo.png"
+            height="48"
+            width="48"
+            alt="Logo"
+          />
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-grey-900">
+            Sign in to your account
+          </h2>
+          <AuthForm />
+        </div>
+      </div>
+    </>
   );
 };
 
